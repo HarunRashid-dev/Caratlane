7cc9ae372d2376fecbfc9794894e6c9c003f5662
HarunRashid-dev <harunrashid9159@gmail.com>
2025-03-24 08:33:10 +0530

    Working in logo

--- app/src/main/java/com/example/caratlane/MainActivity.kt
+++ app/src/main/java/com/example/caratlane/MainActivity.kt
@@ -3,17 +3,7 @@ package com.example.caratlane
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.caratlane.ui.theme.Caratlane
-import com.example.caratlane.ui.theme.CaratlaneTheme
 
 class MainActivity : ComponentActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
--- app/src/main/java/com/example/caratlane/ui/theme/Caratlane Logo.kt
+++ app/src/main/java/com/example/caratlane/ui/theme/Caratlane Logo.kt
@@ -1,14 +1,25 @@
 package com.example.caratlane.ui.theme
 
+import android.R.attr.contentDescription
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.caratlane.R
 
 @Preview(showBackground = true)
 @Composable
 fun Caratlane() {
     Image(painter = painterResource(id = R.drawable.logo),
-        contentDescription = "Logo")
+        contentDescription = "Logo",
+        modifier = Modifier
+        .size(60.dp)
+        .padding(8.dp)
+            .align(Alignment.TopStart)
+    )
 }
