0978c7babcbaeacc26a3ffc43069ab3cfbd9724f
HarunRashid-dev <harunrashid9159@gmail.com>
2025-03-24 09:38:47 +0530

    Working in logo

--- app/src/main/java/com/example/caratlane/ui/theme/Caratlane Logo.kt
+++ app/src/main/java/com/example/caratlane/ui/theme/Caratlane Logo.kt
@@ -1,25 +1,51 @@
 package com.example.caratlane.ui.theme
 
-import android.R.attr.contentDescription
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ModifierLocalBeyondBoundsLayout
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.caratlane.R
 
 @Preview(showBackground = true)
 @Composable
-fun Caratlane() {
-    Image(painter = painterResource(id = R.drawable.logo),
-        contentDescription = "Logo",
-        modifier = Modifier
-        .size(60.dp)
-        .padding(8.dp)
-            .align(Alignment.TopStart)
-    )
+fun Caratlane (modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.Start,
+        verticalArrangement = Arrangement.Top,
+    ){
+        Image(painter = painterResource(id = R.drawable.logo),
+            contentDescription = "Logo",
+            modifier = Modifier.size(50.dp)
+        )}
+
+        Spacer(modifier = Modifier.height(40.dp))
+    Row(
+        modifier = modifier.fillMaxSize(),
+    ){
+        Text(
+            text = "Delivery & Store Near You",
+            fontSize = 10.sp,
+            fontWeight = FontWeight.Bold,
+            color = Color.Gray,
+            modifier = Modifier
+            .padding(start =55.dp)
+        )
+    }
 }
