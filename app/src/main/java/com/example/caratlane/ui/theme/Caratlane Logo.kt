750a61bae7a3795f794855d1038cd64e2792638e
HarunRashid-dev <harunrashid9159@gmail.com>
2025-03-25 09:45:27 +0530

    Working 3 Day

--- app/src/main/java/com/example/caratlane/ui/theme/Caratlane Logo.kt
+++ app/src/main/java/com/example/caratlane/ui/theme/Caratlane Logo.kt
@@ -9,57 +9,69 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.runtime.*
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ModifierLocalBeyondBoundsLayout
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.caratlane.R
+import androidx.compose.ui.text.input.KeyboardType
+
+
 
 @Preview(showBackground = true)
 @Composable
-fun Caratlane (modifier: Modifier = Modifier) {
+fun Caratlane(modifier: Modifier = Modifier){
+    var pincode by remember { mutableStateOf("") }
+
     Column(
-        modifier = modifier.fillMaxSize(),
+        modifier = modifier
+            .fillMaxSize()
+            .padding(16.dp),
         horizontalAlignment = Alignment.Start,
-        verticalArrangement = Arrangement.Top,
+        verticalArrangement = Arrangement.Top
     ){
-        Image(painter = painterResource(id = R.drawable.logo),
+        Image(
+            painter = painterResource(id = R.drawable.logo),
             contentDescription = "Logo",
             modifier = Modifier.size(50.dp)
-        )}
-
-        Spacer(modifier = Modifier.height(40.dp))
-    Row(
-        modifier = modifier.fillMaxSize(),
-    ){
-        Text(
-            text = "Delivery & Store Near You",
-            fontSize = 10.sp,
-            fontWeight = FontWeight.Bold,
-            color = Color.Gray,
-            modifier = Modifier
-            .padding(start =55.dp)
         )
 
-        Spacer(modifier = Modifier.height(12.dp))
+        Spacer(modifier = Modifier.height(20.dp))
 
-        OutlinedTextField(
-            value = pincode,
-            onValueChange = {pincode = it},
-            lable = { Text("Enter Pincode")},
-            keyboardOptions = KeyboardOptions.Default.copy(
-                KeyboardType = KeyboardType.Number
+        Row(
+            verticalAlignment = Alignment.CenterVertically
+        ){
+            Text(
+                text = "Delivery & Store Near you",
+                fontSize = 14.sp,
+                fontWeight = FontWeight.Bold,
+                color = Color.Gray,
+                modifier = Modifier.padding(end= 10.dp)
             )
 
-        )
+            val keyboaardType = null
+            OutlinedTextField(
+                value = pincode,
+                onValueChange = { pincode = it },
+                label = { Text("Enter Pincode") },
+                keyboardOptions = KeyboardOptions.Default.copy(
+                ),
+                modifier = Modifier.width(150.dp)
+            )
+        }
+
     }
+
 }
