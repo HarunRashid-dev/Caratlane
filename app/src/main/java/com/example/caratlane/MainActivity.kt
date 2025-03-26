7aeb56d2abed12a19657a6bf604263aa8353cee0
HarunRashid-dev <harunrashid9159@gmail.com>
2025-03-26 16:32:05 +0530

    attempt (Search) 1

--- app/src/main/java/com/example/caratlane/MainActivity.kt
+++ app/src/main/java/com/example/caratlane/MainActivity.kt
@@ -1,38 +1,151 @@
 package com.example.caratlane
 
-import android.media.tv.TvContract.Channels.Logo
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowDropDown
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material.icons.filled.ShoppingCart
+import androidx.compose.material3.Icon
+import androidx.compose.material3.SearchBar
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.ComposableTarget
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import java.nio.file.WatchEvent
 
 class MainActivity : ComponentActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            DisplayImage()
+            MainScreen()
         }
     }
 }
 
 @Composable
-fun DisplayImage(){
-    Image(
-        painter = painterResource(id= R.drawable.logo),
-        contentDescription = "App Logo"
-    )
+fun MainScreen(){
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp)
+    ){
+        HeaderSection()
+
+        Spacer(modifier = Modifier.height(1.dp))
+
+        SearchBar()
+    }
 }
+
+@Composable
+fun HeaderSection(){
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceBetween
+    ){
+        Row(verticalAlignment = Alignment.CenterVertically){
+            Image(
+                painter = painterResource(id= R.drawable.logo),
+                contentDescription = "App Logo",
+                modifier = Modifier.size(50.dp)
+            )
+            Spacer(modifier = Modifier.width(8.dp))
+
+            Column {
+                Text(
+                    text = "Delivery & Stores",
+                    fontSize = 14.sp,
+                    fontWeight = FontWeight.Bold,
+                    color = Color(0xFF6B44C5)
+                )
+
+                Row(verticalAlignment = Alignment.CenterVertically){
+                    Text(
+                        text = "Enter Pincode",
+                        fontSize = 12.sp,
+                        color = Color.Black
+                    )
+                    Icon(
+                        imageVector = Icons.Default.ArrowDropDown,
+                        contentDescription = "Dropdown"
+                    )
+                }
+            }
+        }
+
+        Row(verticalAlignment = Alignment.CenterVertically){
+            Icon(
+                imageVector = Icons.Default.Favorite,
+                contentDescription = "Favorite",
+                tint = Color(0xFF6B3FA0),
+                modifier = Modifier.size(24.dp)
+            )
+
+            Spacer(modifier = Modifier.width(12.dp))
+
+            Icon(
+                imageVector = Icons.Filled.ShoppingCart,
+                contentDescription = "Cart",
+                tint = Color(0xFF6B3FA0),
+                modifier = Modifier.size(24.dp)
+            )
+        }
+        }
+}
+
+@Composable
+fun SearchBar(){
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 16.dp)
+            .background(Color(0xFFF5F5F5), shape = RoundedCornerShape(16.dp))
+            .padding(12.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ){
+        Icon(
+            imageVector = Icons.Filled.Search,
+            contentDescription = "Search Icon",
+            tint = Color.Gray,
+            modifier = Modifier.size(20.dp)
+        )
+
+        Spacer(modifier = Modifier.width(8.dp))
+
+        Text(
+            text = "Search 'Price'",
+            color = Color.Gray,
+            fontSize = 14.sp,
+            style = TextStyle()
+        )
+    }
+}
+
+
 
 
 
