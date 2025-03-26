dcb8f4f12c62767e06885ab4cd5f5d028a300f67
HarunRashid-dev <harunrashid9159@gmail.com>
2025-03-26 09:09:14 +0530

    attempt (logo) 5

--- app/src/main/java/com/example/caratlane/MainActivity.kt
+++ app/src/main/java/com/example/caratlane/MainActivity.kt
@@ -1,19 +1,38 @@
 package com.example.caratlane
 
+import android.media.tv.TvContract.Channels.Logo
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import com.example.caratlane.ui.theme.Caratlane
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.ComposableTarget
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Caratlane()
+            DisplayImage()
         }
     }
 }
 
+@Composable
+fun DisplayImage(){
+    Image(
+        painter = painterResource(id= R.drawable.logo),
+        contentDescription = "App Logo"
+    )
+}
 
 
 
